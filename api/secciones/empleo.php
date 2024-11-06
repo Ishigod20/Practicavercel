a95be8007f1477159e8175c1b96d63b990b61ee9
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:56:09 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La afiliación crece en octubre, pero aumentan los demandantes de empleo",
+"autor" => "Anabel Cuevas Vega",
+"resumen" => "La contratación temporal e indefinida aumentó un 8% en octubre.",
 ];
 ?>
