ddc6b59d6cffc6e5839d4dc687948b0629a84b45
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 12:03:29 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenStack: todo listo para dar servicio ",
+"autor" => "Celia Valdeolmillos",
+"resumen" => "a subida de precios y los cambios en las condiciones que están experimentando no pocos clientes de VMware.",
 ];
 ?>
