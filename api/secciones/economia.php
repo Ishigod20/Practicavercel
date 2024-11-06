2e967900c1cf0ae1d7eb0b6e063e826e12e3b1a7
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:35:24 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $economia = [
 "titulo" => "La situación económica ha sido el factor clave de la victoria de Trump",
 "autor" => "Victoriano Sánchez",
-"resumen" => "En una entrevista, una señora latina comentó que el precio de una docena de huevos había subido de 8 a 20 dólares",
+"resumen" => "En una entrevista, una señora latina comentó que el precio de una docena de huevos había subido de 8 a 20 dólares".,
 ];
 ?>
