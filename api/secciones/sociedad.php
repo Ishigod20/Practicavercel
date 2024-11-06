49cce608a8017500a7b2e4091506b635db97151c
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 12:04:27 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Además del mensaje de alerta",
+"autor" => "DIANA",
+"resumen" => "El buen funcionamiento de los sistemas de aviso temprano.",
 ];
 ?>
