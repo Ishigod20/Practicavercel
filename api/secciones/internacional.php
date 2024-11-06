d7bc215001dce03aca10a9cbfa67a76d86d1cacc
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:26:58 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
+"titulo" => "Buenas",
 "autor" => "XXX",
 "resumen" => "XXX",
 ];
