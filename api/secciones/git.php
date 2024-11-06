6f707161e026c33dfd4b42c856c63d4fa135f850
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 12:00:36 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Brecha masiva en Git: 15,000 Credenciales Robadas y 10,000 Repositorios Privados en Riesgo",
+"autor" => "Pedro Fernandez ",
+"resumen" => "Los atacantes aprovecharon estas configuraciones para robar credenciales y clonar repositorios privados. El ataque, conocido como EMERALDWHALE, afectó a más de 10.000 repositorio.",
 ];
 ?>
