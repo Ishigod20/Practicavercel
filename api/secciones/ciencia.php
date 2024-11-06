381cd0e2952dc749b25b66db77b244c8cf290a11
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:48:41 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Por qué lo que pasa en el Polo Norte puede generar horribles tormentas en España",
+"autor" => "Nuño Domínguez",
+"resumen" => "La ciencia intenta entender cómo el calentamiento desbocado del Ártico puede provocar eventos de tiempo extremo como la dana de Valencia",
 ];
 ?>
