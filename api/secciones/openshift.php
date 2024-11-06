a64e5a9b7a88d3585a2856217784188e5cea3760
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 12:02:12 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat OpenShift responde a las altas demandas de la OMS",
+"autor" => "OMS",
+"resumen" => "La solución, fue diseñar una plataforma de código abierto. Por ello, la OMS trabajó con Red Hat Open Innovation Labs, que es un programa intensivo de 8 semanas.",
 ];
 ?>
