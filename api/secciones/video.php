7b665bb1c2e4ccc8949ada2a92191349105bdc87
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 12:05:19 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "MIRA, Magaly TV La Firme 2024 EN VIVO: Sigue el programa de HOY, 5 de noviembre",
+"autor" => "EL COMERCIO",
+"resumen" => "Regresó a la pantalla chica peruana este año 2024 con lo más reciente de la farándula y el mundo del espectáculo",
 ];
 ?>
