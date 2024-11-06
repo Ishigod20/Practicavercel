2a49564c78e4312efb4ffd8bd072fea7796aa958
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:29:25 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "Buenas",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Cómo Donald Trump volvió del abismo político",
+"autor" => "Anthony Zurcher",
+"resumen" => "Su primer mandato terminó en caos y con una condena social incluso por parte de miembros de su propio partido.",
 ];
 ?>
