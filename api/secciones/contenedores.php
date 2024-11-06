41817d5e8cd72bbb9f037ecd1cab06a1d15f30fa
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:59:29 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Alan García: lo que pensaba sobre su vida, la política y sus críticos, cuando Somos lo entrevistó en 2014",
+"autor" => "Mireya Peredo",
+"resumen" => "A pocos días de cumplir 65 años, Alan García recibió a Marisol Grau, entonces periodista de Somos para revisar episodios del pasado y del presente, en la política y en su vida privada.",
 ];
 ?>
