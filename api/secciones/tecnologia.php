46da0a5cd9de04829f087146d073eec1261ff6c8
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:44:11 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Interbank ante filtración de datos de usuarios",
+"autor" => "Claudia Inga Martínez",
+"resumen" => ". El ‘hacker’ afirmaba tener más de 3,7 terabytes (TB) de datos de tres millones de usuarios de Interbank.",
 ];
 ?>
