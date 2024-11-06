0d76d515aa84effcd936e1664cc6d513338da9eb
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:53:55 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Agatha, ¿quién si no?' era una miniserie pero podría dejar de serlo en breve",
+"autor" => "Pere Solà Gimferrer Barcelona",
+"resumen" => "La guionista Jac Schaeffer, al igual que hizo con Bruja Escarlata y Visión, supuestamente escribió Agatha, ¿quién si no? como una miniserie.",
 ];
 ?>
