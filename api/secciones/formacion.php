117908c96759ccd30054299774ef58dc75d3fa37
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:58:00 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "UNS lanza nuevo programa de inteligencia artificial para estudiantes",
+    "autor" => "sistemas@uns.edu",
+    "resumen" => "La Universidad Nacional del Sur (UNS) ha lanzado un programa especializado en inteligencia artificial que ofrece a los estudiantes conocimientos avanzados en IA."
 ];
 ?>
