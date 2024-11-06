ddf8114e826de326b0d7627431436b17eb8e5461
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:38:45 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -6,5 +6,5 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $opinion = [
 "titulo" => "Presidenta Boluarte: “Avanzamos en fortalecer capacidad de respuesta de nuestras FF.AA.”",
 "autor" => "gob.pe",
-"resumen" => "Hoy presentamos el primer avión ambulancia, de los dos que hemos adquirido para nuestra querida fuerza aérea.",]
+"resumen" => "Hoy presentamos el primer avión ambulancia, de los dos que hemos adquirido para nuestra querida fuerza aérea."];
 ?>
