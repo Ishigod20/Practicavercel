2e0735894d3ee43ded7d1dc82faeaada4e6ea9c4
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:31:53 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Precio del dólar hoy en Perú: ¿cuánto está el tipo de cambio este miércoles 6 de noviembre?",
+"autor" => "cuantoestaeldolar.pe",
+"resumen" => "El tipo de cambio corresponde al cierre del martes 5 de noviembre, reportado por el Banco Central de Reserva del Perú. Es decir, el precio del dólar se mantuvo en S/3.774.",
 ];
 ?>
