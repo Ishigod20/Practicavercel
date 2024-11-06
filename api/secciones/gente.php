69ab595a7777d8267c96161536d6963e0bb9f709
Ishigod20 <119701613+Ishigod20@users.noreply.github.com>
2024-11-06 11:52:41 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Los planes de Elon Musk para ir a Marte… dentro de dos años",
+"autor" => "RAFAEL CLEMETE",
+"resumen" => "Sus declariciones fueron clasras y concisas",
 ];
 ?>
